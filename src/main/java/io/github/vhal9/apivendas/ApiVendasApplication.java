93b4bb3dfe994ec7e5462462502f4824bfc15129
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-05-24 12:52:30 -0300

    iniciando projeto

---
+++ src/main/java/io/github/vhal9/apivendas/ApiVendasApplication.java
@@ -0,0 +1,13 @@
+package io.github.vhal9.apivendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiVendasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/vhal9/apivendas/ApiVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vhal9.apivendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
