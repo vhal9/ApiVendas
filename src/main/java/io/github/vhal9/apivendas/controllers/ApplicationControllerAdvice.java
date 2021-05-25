0b77c54aa64c74915dfdbeb3eb7b0ef6cd1394cc
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-05-24 23:18:06 -0300

    adiciona exception handler para lidar com exceção do tipo PedidoNaoEncontradoException

--- src/main/java/io/github/vhal9/apivendas/controllers/ApplicationControllerAdvice.java
+++ src/main/java/io/github/vhal9/apivendas/controllers/ApplicationControllerAdvice.java
@@ -1,5 +1,6 @@
 package io.github.vhal9.apivendas.controllers;
 
+import io.github.vhal9.apivendas.exceptions.PedidoNaoEncontradoException;
 import io.github.vhal9.apivendas.exceptions.RegraDeNegocioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,14 @@ public class ApplicationControllerAdvice {
     @ExceptionHandler(RegraDeNegocioException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErros handleRegraDeNegocioException(RegraDeNegocioException ex) {
+
+        return new ApiErros(ex.getMessage());
+
+    }
+
+    @ExceptionHandler(PedidoNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErros handlePedidoNotFoundException(PedidoNaoEncontradoException ex){
 
         return new ApiErros(ex.getMessage());
 
